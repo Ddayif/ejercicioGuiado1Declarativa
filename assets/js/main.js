4430dd20ad58ed24b5ec42b9dd39a646a90416b0
Joselin Grez <joselingrez.m@hotmail.es>
2017-05-09 23:05:10 -0300

    Agregando funcion de la forma imperativa

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,5 @@
+//forma declarativa
+
 function intersectionSet2(l1, l2) {
 	return l1.filter(function(n){
 		return l2 .indexOf(n) !== -1
@@ -5,3 +7,18 @@ function intersectionSet2(l1, l2) {
 };
 
 intersectionSet2([1,2,3], [2,3,4]);
+
+
+
+//forma imperativa
+function intersection(l1, l2){
+	var l3=[];
+	for(var i=0; i< l1.length; i++){
+		for(var j=0; j< l2.length; j++){
+			if(l1[i]==l2[j]){
+				l3.push(l1[i]);
+			}
+		}
+	}
+	return l3;
+}
